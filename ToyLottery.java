474d58a8d920cf668673121a1260a26a7ea781da
Aliona <aljereghi@gmail.com>
2024-02-02 00:29:48 +0200

    Created ToyLottery class

--- ToyLottery.java
+++ ToyLottery.java
@@ -14,3 +14,25 @@ class Toy {
         this.frequency = frequency;
     }
 }
+
+public class ToyLottery {
+    // Создаем коллекцию для хранения всех игрушек в магазине
+    private static final List<Toy> toysInStore = new ArrayList<>();
+
+    // Создаем приоритетную очередь для хранения всех игрушек в магазине
+    private static final PriorityQueue<Toy> toyQueue = new PriorityQueue<>(Comparator.comparingInt(Toy::hashCode));
+
+    // Создаем коллекцию для хранения выигранных призов
+    private static final List<String> prize = new ArrayList<>();
+
+    // Создаем генератор случайных чисел
+    private static final Random random = new Random();
+
+    public static void main(String[] args) {
+        // Инициализируем магазин изначальными товарами
+        Toy[] toys = {
+                new Toy(1, "конструктор", 20),
+                new Toy(2, "робот", 20),
+                new Toy(3, "кукла", 60)
+        };
+        
